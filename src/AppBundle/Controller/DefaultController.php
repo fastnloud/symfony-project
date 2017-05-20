af61cdae6bc4606ad2795fb44a8996b84a7339b9
jaap <thinkfast22>
2017-05-20 16:12:59 +0200

    Populating template with view variables.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,10 +2,18 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\ViewVariable;
+use AppBundle\Repository\WorkExperienceRepository;
+use AppBundle\Repository\EducationRepository;
+use AppBundle\Repository\SkillRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Class DefaultController
+ * @package AppBundle\Controller
+ */
 class DefaultController extends Controller
 {
     /**
@@ -13,9 +21,28 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        $vars = [];
+
+        // fetch global view variables
+        $text = $this->getDoctrine()->getRepository('AppBundle:ViewVariable')->findAll();
+
+        /* @var $obj ViewVariable */
+        foreach ($text as $obj) {
+            $vars[$obj->getName()] = $obj->getValue();
+        }
+
+        /** @var WorkExperienceRepository $repository */
+        $repository = $this->getDoctrine()->getRepository('AppBundle:WorkExperience');
+        $vars['work_experience_list'] = $repository->findAllOrderedByPriority();
+
+        /** @var EducationRepository $repository */
+        $repository = $this->getDoctrine()->getRepository('AppBundle:Education');
+        $vars['education_list'] = $repository->findAllOrderedByPriority();
+
+        /** @var SkillRepository $repository */
+        $repository = $this->getDoctrine()->getRepository('AppBundle:Skill');
+        $vars['skill_list'] = $repository->findAllOrderedByRand();
+
+        return $this->render('default/index.html.twig', $vars);
     }
 }
